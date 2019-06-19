cde35504778e01200a3e2982cb085366cd1c5647
Ken Yin <kyin@cumulusnetworks.com>
2019-06-19 14:55:00 -0400

    Adding files used to setup the virtual environment
    Ticket: None
    Reviewed By: None
    Testing Done: Created a brand new virtual environment and checked dependencies
    
    Adding some of the files that the create_virtual_env.py script will
    used to build the virtual environment

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='pydotsim',
+      version='0.0.1',
+      description='',
+      author='Kenneth Yin',
+      author_email='kyin98@yahoo.com',
+      packages=find_packages(),
+      install_requires=["pydot==1.2.2",
+                        "ipython==4.2.1",
+                        "ipdb==0.9.3",
+                        "ipaddress==1.0.22"],
+      zip_safe=False
+)
