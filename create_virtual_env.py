8bc3543dbe921124cc06764cebcb95ad28491f2f
Ken Yin <kyin@cumulusnetworks.com>
2019-06-19 14:45:00 -0400

    Adding script to create the virtual environment
    Ticket: None
    Reviewed By: None
    Testing Done: Created and tested a virtual environment
    
    In order to simplify things in the pydotsim, a virtual environment
    can be created with all the libraries setup to run pydotsim.
    The default directory that the virtual environment is created in is
    the .venv directory in the current working directory.  If you
    call 'create_virtual_env.py' with the '-h' option, it will print
    out help.

---
+++ create_virtual_env.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import subprocess
+import sys
+import logging
+import shlex
+import apt
+import apt_inst
+import apt_pkg
+
+
+def create_ipython_wrapper(base_dir):
+    ipython_file = """#!/bin/bash\n"exec" "{0}/bin/python" "$0" "$@" "--ipython-dir={0}/.ipython"\n""".format(base_dir) + \
+                   """# -*- coding: utf-8 -*-\nimport re\nimport sys\n\nfrom IPython import start_ipython\n\n""" + \
+                   """if __name__ == '__main__':\n     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])\n""" + \
+                   """     sys.exit(start_ipython())"""
+
+    with open('{0}/bin/ipython'.format(base_dir), 'w') as stream:
+        stream.write(ipython_file)
+
+def main():
+    parser = argparse.ArgumentParser(description='Setup user environment')
+    parser.add_argument('--loglevel', help='Logging level', choices=['DEBUG',
+                        'ERROR', 'INFO', 'WARN'])
+    parser.add_argument('--vdir', help='Directory to create the virtual environment')
+
+    args = parser.parse_args()
+
+    if args.vdir:
+        base_dir = os.path.abspath(args.vdir)
+    else:
+        base_dir = os.getcwd() + '/.venv'
+
+    cmds = ['virtualenv {0}'.format(base_dir)]
+
+    # Link the apt libraries in the virtual environment
+    sitepkgs = "{0}/lib/python2.7/site-packages/".format(base_dir)
+    apt_libs = [os.path.dirname(apt.__file__)]
+    apt_libs.append( apt_inst.__file__)
+    apt_libs.append(apt_pkg.__file__)
+
+    for apt_file in apt_libs:
+        cmds.append("ln -s {0} {1}".format(apt_file, sitepkgs))
+
+    cmds.append('{0}/bin/pip install --no-cache-dir -q -r requirements.txt'.format(base_dir))
+
+    for cmd in cmds:
+        subprocess.check_output(shlex.split(cmd))
+
+    create_ipython_wrapper(base_dir)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)5s: %(message)s')
+    log = logging.getLogger(__name__)
+    sys.exit(main())
