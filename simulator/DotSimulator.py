9b6e7d8653dc0b4c60c1d077f71232e57eeafffe
Ken Yin <kyin@cumulusnetworks.com>
2019-07-14 22:07:09 -0400

    Add ImageDepot into base class
    Ticket: None
    Reviewed By: None
    Testing Done: None
    
    Adding the ImageDepot class to search for the VM images to use.

--- simulator/DotSimulator.py
+++ simulator/DotSimulator.py
@@ -8,16 +8,37 @@ import importlib
 import pkgutil
 import inspect
 import apt
+import argparse
+import random
 from simulator.builders import BuilderBase, BuilderSelector
+fro simulator.utilities.ImageDepot import ImageDepot
 from collections import OrderedDict
 from logging import getLogger
 
-log = getLogger()
+log = getLogger(__name__)
 
 
 class DotSimulator(object):
-    def __init__(self):
-        self.builder = BuilderSelector(iself.graph).builder
+    def __init__(self, **kwargs):
+        # Set the simulation directory
+        if 'sim_dir' in kwargs:
+            self.sim_dir = kwargs['sim_dir']
+        else:
+            unique_id = "".join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(5))
+            self.sim_dir = '/tmp/{0}/{1}-{2}/'.format(os.getusername(), self.__class__.__name__, unique_id)
+
+        # Set the Image Depot directory
+        if 'image_depot' in kwargs:
+            self.image_depot_dir = kwargs['image_depot']
+        else:
+            self.image_depot_dir = '/media/psf/image_depot'
+
+        self.image_depot = ImageDepot(self.image_depot_dir)
+
+        # The class inheriting DotSimulator should also be
+        # inheriting from DotTopo.  This is where self.graph
+        # is defined.
+        self.builder = BuilderSelector(self.graph, self.sim_dir, self.image_depot).builder
 
     def configure(self):
         pass
@@ -27,3 +48,6 @@ class DotSimulator(object):
 
     def stop(self):
         self.builder.stop()
+
+    def run_from_cmdline(self):
+        pass
